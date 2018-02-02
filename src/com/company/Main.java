0698f64881424016e910f724fed268ff5dfe20a4
Luke <Lyspeers@gmail.com>
2018-02-01 22:51:08 -0500

    Everything Works

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String temp1 = "", temp2 = "", temp3 = "";
+        int temp1 = 0, temp2 = 0, temp3 = 0;
         Scanner input = new Scanner(System.in);
         Scanner number = new Scanner(System.in);
         System.out.println("Are you entering a Hex code(hex) an octal code(octal) or and rgb code(rgb)");
@@ -12,26 +12,38 @@ public class Main {
 
 
         if(type.equalsIgnoreCase("hex")){
-            System.out.println("Please enter your number to be converted in groups of 2. ex. ff,7a,02");
-            String hex = number.next();
-            for(int i = 0;i<hex.length();i+=2){
-                if(i<2)
-                    temp1 = hex.substring(i, i+2);
-                else if(i<4)
-                    temp2 = hex.substring(i, i+2);
-                else if(i<6)
-                    temp3 = hex.substring(i, i+2);
-                else
-                    System.out.println("Please make sure you are imputing a valid 6 digit hex code");
-            }
-            System.out.println(temp1 +"," +temp2 + "," + temp3);
+            System.out.println("Please the R group of your hex code");
+            temp1 = number.nextInt(16);
+            System.out.println("Please the G group of your hex code");
+            temp2 = number.nextInt(16);
+            System.out.println("Please the B group of your hex code");
+            temp3 = number.nextInt(16);
+
+            System.out.println(temp1 +"," +temp2 + "," + temp3 + " This is your rgb color code");
+            System.out.println(Integer.toOctalString(temp1) +"," +Integer.toOctalString(temp2) + "," + Integer.toOctalString(temp3) + " This is your octal code");
 
         }else if(type.equalsIgnoreCase("octal")){
-            System.out.println("Please enter your number to be converted in groups of 3 ex. 377,255,089");
-            int octal = number.nextInt();
+            System.out.println("Please the R group of your octal code");
+            temp1 = number.nextInt(8);
+            System.out.println("Please the G group of your octal code");
+            temp2 = number.nextInt(8);
+            System.out.println("Please the B group of your octal code");
+            temp3 = number.nextInt(8);
+
+            System.out.println(temp1 +"," +temp2 + "," + temp3 + " This is your rgb color code");
+            System.out.println("#" + Integer.toHexString(temp1) + Integer.toHexString(temp2) +   Integer.toHexString(temp3) + " This is your hex code");
+
         }else if(type.equalsIgnoreCase("rgb")){
-            System.out.println("Please enter your number to be converted in groups of 3 ex. 255,65,23");
-            int rgb = number.nextInt();
+            System.out.println("Please the R group of your code");
+            temp1 = number.nextInt();
+            System.out.println("Please the G group of your code");
+            temp2 = number.nextInt();
+            System.out.println("Please the B group of your code");
+            temp3 = number.nextInt();
+
+            System.out.println(Integer.toOctalString(temp1) +"," +Integer.toOctalString(temp2) + "," + Integer.toOctalString(temp3) + " This is your octal code");
+            System.out.println("#" + Integer.toHexString(temp1) + Integer.toHexString(temp2) +   Integer.toHexString(temp3) + " This is your hex code");
+
         }else{
             System.out.println("Please make sure you input the correct code");
         }
